c9dd3e7cf23abc81a5510a391902d8a65567c7f5
unknown <vimanyu@seas.upenn.edu>
2013-11-09 04:49:10 -0500

    attempt for cubemap

--- part2/frag_globe.js
+++ part2/frag_globe.js
@@ -57,12 +57,33 @@
     var u_timeLocation;
     var u_resInverseLocation;
 
+    var u_cubeModelViewLocation;
+    var u_cubePerspLocation;
+    var cubeVPositionLocation;
+    var cubeVTexCoordLocation;
+    var u_CubeTransformMatrixLocation;
+    var u_CubeTextureLocation;
+
+    var program ;
+    var cubeMapProgram;
 
     (function initializeShader() {
         var vs = getShaderSource(document.getElementById("vs"));
         var fs = getShaderSource(document.getElementById("fs"));
+        var vs_cubemap = getShaderSource(document.getElementById("vs_cubemap"));
+        var fs_cubemap = getShaderSource(document.getElementById("fs_cubemap"));
 
-        var program = createProgram(gl, vs, fs, message);
+        cubeMapProgram = createProgram(gl, vs_cubemap, fs_cubemap, message);
+        gl.useProgram(cubeMapProgram);
+        cubeVPositionLocation = gl.getAttribLocation(cubeMapProgram, "CubeVPosition");
+        //cubeVTexCoordLocation = gl.getAttribLocation(cubeMapProgram, "CubeVTexCoord");
+        u_CubeTransformMatrixLocation = gl.getUniformLocation(cubeMapProgram,"u_CubeTransMat");
+        u_cubeModelViewLocation = gl.getUniformLocation(cubeMapProgram,"u_ModelView");
+        u_cubePerspLocation = gl.getUniformLocation(cubeMapProgram,"u_Persp");
+        u_CubeTextureLocation= gl.getUniformLocation(cubeMapProgram,"u_CubeTexture");
+
+        program = createProgram(gl, vs, fs, message);
+        gl.useProgram(program);
         positionLocation = gl.getAttribLocation(program, "Position");
         normalLocation = gl.getAttribLocation(program, "Normal");
         texCoordLocation = gl.getAttribLocation(program, "Texcoord");
@@ -80,15 +101,39 @@
         u_CameraSpaceDirLightLocation = gl.getUniformLocation(program,"u_CameraSpaceDirLight");
         u_resInverseLocation = gl.getUniformLocation(program,"u_resInverse");
             
-        gl.useProgram(program);
     })();
 
-    var dayTex   = gl.createTexture();
-    var bumpTex  = gl.createTexture();
+    var dayTex = gl.createTexture();
+    var bumpTex = gl.createTexture();
     var cloudTex = gl.createTexture();
     var transTex = gl.createTexture();
     var lightTex = gl.createTexture();
-    var specTex  = gl.createTexture();
+    var specTex = gl.createTexture();
+
+    var cubeTex = gl.createTexture();
+    var cubeFrontTex = gl.createTexture();
+    var cubeBackTex = gl.createTexture();
+    var cubeLeftTex = gl.createTexture();
+    var cubeRightTex = gl.createTexture();
+    var cubeTopTex = gl.createTexture();
+    var cubeBottomTex = gl.createTexture();
+
+
+    function initCubeMapTexture(texture,ftex,btex,ltex,rtex,ttex,bottomtTex){
+        gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.REPEAT);
+        gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    
+        gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Z, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, ftex.image);
+        gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, btex.image);
+        gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, rtex.image);
+        gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, ltex.image);
+        gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITVE_Y, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, ttex.image);
+        gl.texImage3D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, bottomTex.image);
+    }   
 
     function initLoadedTexture(texture){
         gl.bindTexture(gl.TEXTURE_2D, texture);
@@ -101,33 +146,69 @@
         gl.bindTexture(gl.TEXTURE_2D, null);
     }
 
+    var cubePositionsName = gl.createBuffer();
+    var cubeIndicesName = gl.createBuffer();
+    var cubeTexCoordsName = gl.createBuffer();
+    (function initializeCubeMap(){
+
+        var numberOfPoints = 4;
+        var positions = new Float32Array(2*numberOfPoints);
+        positions[0] = -1.0; positions[1] = -1.0;
+        positions[2] =  1.0; positions[3] = -1.0;
+        positions[4] =  1.0; positions[5] =  1.0;
+        positions[6] = -1.0; positions[7] = 1.0;
+        
+
+        var texCoords = new Float32Array(2 * numberOfPoints);
+        texCoords[0] =  1.0; texCoords[1] =  1.0;
+        texCoords[2] =  0.0; texCoords[3] =  1.0;
+        texCoords[4] =  0.0; texCoords[5] =  0.0;
+        texCoords[6] =  1.0; texCoords[7] =  1.0;
+        
+        var indices = new Uint16Array(6);
+        indices[0] = 0; indices[1] = 1; indices[2]=3; indices[3]=3;
+        indices[4] = 1; indices[5] = 2; 
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, cubePositionsName);
+        gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+        gl.vertexAttribPointer(cubeVPositionLocation, 2, gl.FLOAT, false, 0, 0);
+
+        //gl.bindBuffer(gl.ARRAY_BUFFER, cubeTexCoordsName);
+        //gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
+        //gl.vertexAttribPointer(cubeVTexCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndicesName);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+    })();
+
+
     var numberOfIndices;
+    var positionsName = gl.createBuffer();
+    var normalsName = gl.createBuffer();
+    var texCoordsName = gl.createBuffer();
+    var indicesName = gl.createBuffer();
 
     (function initializeSphere() {
         function uploadMesh(positions, texCoords, indices) {
             // Positions
-            var positionsName = gl.createBuffer();
             gl.bindBuffer(gl.ARRAY_BUFFER, positionsName);
             gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
             gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(positionLocation);
+            //gl.enableVertexAttribArray(positionLocation);
             
             // Normals
-            var normalsName = gl.createBuffer();
             gl.bindBuffer(gl.ARRAY_BUFFER, normalsName);
             gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
             gl.vertexAttribPointer(normalLocation, 3, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(normalLocation);
+            //gl.enableVertexAttribArray(normalLocation);
             
             // TextureCoords
-            var texCoordsName = gl.createBuffer();
             gl.bindBuffer(gl.ARRAY_BUFFER, texCoordsName);
             gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
             gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(texCoordLocation);
+            //gl.enableVertexAttribArray(texCoordLocation);
 
             // Indices
-            var indicesName = gl.createBuffer();
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesName);
             gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
         }
@@ -157,7 +238,7 @@
                 positions[positionsIndex++] = Math.sin(inclination)*Math.sin(azimuth);
                 texCoords[texCoordsIndex++] = i / WIDTH_DIVISIONS;
                 texCoords[texCoordsIndex++] = j / HEIGHT_DIVISIONS;
-            } 
+            }
         }
 
         for( var j = 0; j < HEIGHT_DIVISIONS; ++j )
@@ -261,14 +342,54 @@
         vec3.normalize(lightdir);
         var resInverse = vec2.create([1.0/1024.0,1.0/512.0]);
 
+        var invPersp = mat4.create();
+        mat4.inverse(persp, invPersp);
+        var invMV = mat4.create();
+        mat4.inverse(view,invMV);
+        
+        var invMVP = mat4.create();
+        mat4.multiply(invMV,invPersp,invMVP); 
+
+        ///////////////////////////////////////////////////////////////////////////
+        // RenderCubeMap quad
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.useProgram(cubeMapProgram);
+        gl.bindBuffer(gl.ARRAY_BUFFER, cubePositionsName);
+        gl.vertexAttribPointer(cubeVPositionLocation, 2, gl.FLOAT, false, 0, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, cubeTexCoordsName);
+        //gl.vertexAttribPointer(cubeVTexCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndicesName);
+        gl.uniformMatrix4fv(u_cubeModelViewLocation, false, mv);
+        gl.uniformMatrix4fv(u_cubePerspLocation, false, persp);
+        gl.uniformMatrix4fv(u_CubeTransformMatrixLocation,false,invMVP);
+        gl.enableVertexAttribArray(cubeVPositionLocation);
+        gl.enableVertexAttribArray(cubeVTexCoordLocation);
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_CUBE_MAP, cubeTex);
+        gl.uniform1i(u_CubeTextureLocation, 0);
+        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT,0);
+        gl.disableVertexAttribArray(cubeVPositionLocation);
+        gl.disableVertexAttribArray(cubeVTexCoordLocation);
         ///////////////////////////////////////////////////////////////////////////
         // Render
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
+        // Render
+        gl.clear(gl.DEPTH_BUFFER_BIT);
+        gl.useProgram(program);
+        gl.enableVertexAttribArray(positionLocation);
+        gl.enableVertexAttribArray(normalLocation);
+        gl.enableVertexAttribArray(texCoordLocation);
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionsName);
+        gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, normalsName);
+        gl.vertexAttribPointer(normalLocation, 3, gl.FLOAT, false, 0, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, texCoordsName);
+        gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
         gl.uniformMatrix4fv(u_ModelLocation, false, model);
         gl.uniformMatrix4fv(u_ViewLocation, false, view);
         gl.uniformMatrix4fv(u_PerspLocation, false, persp);
         gl.uniformMatrix4fv(u_InvTransLocation, false, invTrans);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesName);
         
         gl.uniform3fv(u_CameraSpaceDirLightLocation, lightdir);
         gl.uniform1f(u_timeLocation,time);
@@ -294,6 +415,9 @@
         gl.uniform1i(u_EarthSpecLocation, 5);
         gl.drawElements(gl.TRIANGLES, numberOfIndices, gl.UNSIGNED_SHORT,0);
 
+        gl.disableVertexAttribArray(texCoordLocation);
+        gl.disableVertexAttribArray(normalLocation);
+        gl.disableVertexAttribArray(positionLocation);
         time += 0.001;
         window.requestAnimFrame(animate);
     }
@@ -313,6 +437,64 @@
         texture.image.src = src;
     }
 
+
+function loadCubeMap(base, suffix) {
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    var faces = [["posx", gl.TEXTURE_CUBE_MAP_POSITIVE_X],
+                 ["negx", gl.TEXTURE_CUBE_MAP_NEGATIVE_X],
+                 ["posy", gl.TEXTURE_CUBE_MAP_POSITIVE_Y],
+                 ["negy", gl.TEXTURE_CUBE_MAP_NEGATIVE_Y],
+                 ["posz", gl.TEXTURE_CUBE_MAP_POSITIVE_Z],
+                 ["negz", gl.TEXTURE_CUBE_MAP_NEGATIVE_Z]];
+    for (var i = 0; i < faces.length; i++) {
+        var url = base + "-" + faces[i][0] + "." + suffix;
+        var face = faces[i][1];
+        var image = new Image();
+        image.onload = function(texture, face, image, url) {
+            return function() {
+                gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+                gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+                gl.texImage2D(
+                   face, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+            }
+        }(texture, face, image, url);
+        image.src = url;
+    }
+    return texture;
+}
+
+
+    function initializeCubeTexture(texture,ftex,fsrc,btex,bsrc,ltex,lsrc,rtex,rsrc,ttex,tsrc,bottomTex,bottomSrc){
+      ftex.image = new Image();
+      btex.image = new Image();
+      ltex.image = new Image();
+      rtex.image = new Image();
+      ttex.image = new Image();
+      bottomTex.image = new Image();
+      var image = new Image();
+      image.onload = function() {
+          initCubeMapTexture(texture,ftex,btex,ltex,rtex,ttex,bottomTex);
+          }
+      ftex.image.src = fsrc;
+      btex.image.src = bsrc;
+      ltex.image.src = lsrc;
+      rtex.image.src = rsrc;
+      ttex.image.src = tsrc;
+      bottomTex.image.src = bottomSrc;
+          
+    }
+   // initializeCubeTexture(cubeTex,cubeFrontTex,"skybox-xpos.jpg",
+   //                       cubeBackTex,"skybox-xneg.jpg",
+   //                       cubeLeftTex,"skybox-ypos.jpg",
+   //                       cubeRightTex,"skybox-yneg.jpg",
+   //                       cubeTopTex,"skybox-zpos.jpg",
+   //                       cubeBottomTex,"skybox-zneg.jpg");
+    cubeTex = loadCubeMap("skybox","jpg");
     initializeTexture(dayTex, "earthmap1024.png");
     initializeTexture(bumpTex, "earthbump1024.png");
     initializeTexture(cloudTex, "earthcloud1024.png");
@@ -320,3 +502,4 @@
     initializeTexture(lightTex, "earthlight1024.png");
     initializeTexture(specTex, "earthspec1024.png");
 }());
+
